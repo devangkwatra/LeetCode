5dab231d1b3556b27708a40aa26dd4a1c7ead8e6
Devang Kwatra <80782417+devangkwatra@users.noreply.github.com>
2023-05-26 01:46:46 +0530

    Create missingNumber.cpp

---
+++ Arrays & Hashing/missingNumber.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int xor1 = 0;
+        int xor2 = 0;
+        int n = nums.size();
+        for(int i = 0; i < n; i++)
+        {
+            xor1 ^= nums[i];
+            xor2 ^= i + 1;
+        }
+        return xor1 ^ xor2;
+    }
+};
